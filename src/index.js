781858c6e0ca8e2eb72c15414f9ecfb3d99f3566
Julczan <julek111222h@gmail.com>
2026-01-09 12:45:42 +0100

    project card component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import "./styles.css";
+import { createCard } from "./modules/projectCard";
 
-console.log("greeting");
+const projectContainer = document.querySelector(".projects");
+
+const project = createCard(
+  "Project name",
+  "Short description of the project",
+  "https://julczan.github.io/battleship_odin/",
+  "https://github.com/Julczan/battleship_odin"
+);
+
+projectContainer.appendChild(project);
---
+++ src/modules/projectCard.js
@@ -0,0 +1,39 @@
+import externalSrc from "/src/assets/icons/open-in-new.svg";
+
+function createCard(name, description, externalHyper, githubHyper, imgSrc) {
+  const screenshot = document.createElement("img");
+  const githubIcon = document.createElement("img");
+  const externalIcon = document.createElement("img");
+  const hyperLinkGit = document.createElement("a");
+  const hyperLinkExt = document.createElement("a");
+  const card = document.createElement("div");
+  const projectName = document.createElement("h2");
+  const projectDesc = document.createElement("p");
+
+  githubIcon.src =
+    "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg";
+  externalIcon.src = externalSrc;
+  projectName.textContent = name;
+  hyperLinkExt.href = externalHyper;
+  hyperLinkGit.href = githubHyper;
+  projectDesc.textContent = description;
+
+  githubIcon.classList.add("icon");
+  externalIcon.classList.add("icon");
+  card.classList.add("card");
+  hyperLinkGit.classList.add("hyper-git");
+  hyperLinkExt.classList.add("hyper-ext");
+
+  hyperLinkGit.appendChild(githubIcon);
+  hyperLinkExt.appendChild(externalIcon);
+
+  card.appendChild(screenshot);
+  card.appendChild(projectName);
+  card.appendChild(projectDesc);
+  card.appendChild(hyperLinkGit);
+  card.appendChild(hyperLinkExt);
+
+  return card;
+}
+
+export { createCard };
